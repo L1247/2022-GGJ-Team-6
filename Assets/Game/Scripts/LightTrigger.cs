28bd8efc66d76108691348e91806189eacacd530
rStar <starlove124760@gmail.com>
2022-01-22 15:57:41 +0800

    add asmdef

---
+++ Assets/Game/Scripts/LightTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class LightTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collider2D)
+        {
+            // check if collider is player
+            // publish event
+        }
+    }
+}
